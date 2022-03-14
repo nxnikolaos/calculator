60e700d5e91ba0c60d89c3d5a01c9352a637fe29
nxnikolaos <nxnatlas@gmail.com>
2022-03-14 14:11:15 +0200

    Base Calculator interface established

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let container = document.createElement('div');
 container.classList.add("container");
 parentDiv.appendChild(container);
 
+let calculatorPanel = document.createElement('div');
+calculatorPanel.classList.add("calculator-panel");
+container.appendChild(calculatorPanel);
 
 function add(a,b){
     return a + b;
@@ -36,3 +39,30 @@ function operate(a,ope,b){
     return result;
 }
 
+let displayPanel = document.createElement('div');
+displayPanel.classList.add('display-panel');
+
+let resultDisplay = document.createElement('p');
+resultDisplay.classList.add('result-display');
+displayPanel.appendChild(resultDisplay);
+resultDisplay.textContent ='5+5=10';
+let clearButton = document.createElement('button');
+clearButton.classList.add('clear-button');
+clearButton.textContent = "Clear";
+displayPanel.appendChild(clearButton);
+calculatorPanel.appendChild(displayPanel);
+
+
+
+let buttonBase = document.createElement('div');
+buttonBase.classList.add('button-base');
+calculatorPanel.appendChild(buttonBase);
+
+let buttonArray = [];
+for(let i=0;i<4;i++){
+    for(let j=0;j<4;j++){
+        buttonArray[i] = document.createElement('button');
+        buttonArray[i].classList.add('panel-button');
+        buttonBase.appendChild(buttonArray[i]);
+    }
+}
